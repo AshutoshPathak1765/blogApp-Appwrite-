c2080167c887049b297208c20432020057d4b031
Ashutosh Kumar Pathak <ashutoshpathak1765@gmail.com>
2024-03-08 11:36:58 +0530

    configured environmental variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-
-import './App.css'
+import "./App.css";
 
 function App() {
- 
+  console.log(import.meta.env.VITE_APPWRITE_URL);
   return (
     <>
       <h1>A blog App with appwrite</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
