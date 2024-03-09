a5351497f6e2ae30695a9fac16d63ddefaf12849
Ashutosh Kumar Pathak <ashutoshpathak1765@gmail.com>
2024-03-09 20:57:10 +0530

    configured redux toolkit and tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,37 @@
-import "./App.css";
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/features/authSlice";
+import { Footer, Header } from "./components";
+import { Outlet } from "react-router-dom";
+import "./App.css"
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
-  return (
-    <>
-      <h1>A blog App with appwrite</h1>
-    </>
-  );
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login(userData));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+  return !loading ? (
+    <div class="min-h-screen flex border-red-500 flex-wrap content-between bg-gray-400">
+    <div class="w-full block">
+      <Header />
+      <main>
+        TODO: <Outlet />
+      </main>
+      <Footer />
+    </div>
+  </div>  
+  ) : null;
 }
 
 export default App;
