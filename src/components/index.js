caeeaf08a9cea2eec19d91a7f08605a82053c624
Ashutosh Kumar Pathak <ashutoshpathak1765@gmail.com>
2024-03-13 10:06:20 +0530

    added all components

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,13 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import Button from "./Button";
+import Input from "./Input";
+import RTE from "./RTE";
+import Signup from "./Signup";
+import Select from "./Select";
+import PostCard from "./PostCard";
 
-export { Header, Footer };
+export { Header, Footer, Container, Logo, LogoutBtn, Button, Input, RTE };
